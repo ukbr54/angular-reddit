cd75f32afa75f7fd2d381d29f86a92f0598b4505
Ujjwal Gupta <ujjwal.kbr@gmail.com>
2020-07-25 17:34:25 +0530

    Added Refresh Token Functionality

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -2,7 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { LoginRequestPayload } from './login-request.payload';
 import { AuthService } from '../shared/auth.service';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { error } from '@angular/compiler/src/util';
+import { throwError } from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -13,12 +16,22 @@ export class LoginComponent implements OnInit {
 
   loginForm: FormGroup;
   loginRequestPayload: LoginRequestPayload;
+  registerSuccessMessage: string;
+  isError: boolean;
 
-  constructor(private authService: AuthService, private router: Router) {
+  constructor(private authService: AuthService, private router: Router,
+        private toastr: ToastrService,private activatedRoute: ActivatedRoute) {
     this.loginRequestPayload = {
       username: '',
       password: ''
     };
+
+    this.activatedRoute.queryParams.subscribe(params => {
+      if(params.registered != undefined && params.registered === 'true'){
+        this.toastr.success('Signup successful');
+        this.registerSuccessMessage = 'Please Check your inbox for activation email activate your account before you Login!';
+      }
+    });
   }
 
   ngOnInit(): void {
@@ -33,7 +46,12 @@ export class LoginComponent implements OnInit {
     this.loginRequestPayload.password = this.loginForm.get('password').value;
 
     this.authService.login(this.loginRequestPayload).subscribe(data => {
-       console.log('Login Successful');
+       this.isError = false;
+       this.router.navigateByUrl('');
+       this.toastr.success('Login Successful');
+    },error =>{
+       this.isError = true;
+       throwError(error);
     });
   }
 
--- src/app/auth/shared/auth.service.ts
+++ src/app/auth/shared/auth.service.ts
@@ -1,16 +1,21 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { SignupRequestPayload } from '../signup/singup-request.payload';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
 import { LoginRequestPayload } from '../login/login-request.payload';
 import { LoginResponse } from '../login/login-response.payload';
 import { LocalStorageService } from 'ngx-webstorage';
-import { map } from 'rxjs/operators';
+import { map, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+
+  refreshTokenPayload = {
+    refreshToken: this.getRefreshToken(),
+    username: this.getUserName()
+  }
 
   constructor(private httpClient: HttpClient,private localStorage: LocalStorageService) { }
 
@@ -29,5 +34,46 @@ export class AuthService {
 
       return true;
     }));
+  }
+
+  getJwtToken(){
+    return this.localStorage.retrieve('authenticationToken');
+  }
+
+  refreshToken(){
+    return this.httpClient.post<LoginResponse>('http://localhost:8080/api/auth/refresh/token',
+          this.refreshTokenPayload).pipe(tap(response => {
+            this.localStorage.clear('authenticationToken');
+            this.localStorage.clear('expiresAt');
+
+            this.localStorage.store('authenticationToken',response.authenticationToken);
+            this.localStorage.store('expireAt', response.expireAt);
+          }));
+  }
+
+  logout(){
+    this.httpClient.post('http://localhost:8080/api/auth/logout',this.refreshTokenPayload,
+      { responseType: 'text' }).subscribe(data => {
+        console.log(data);
+      },error=>{
+        throwError(error);
+      })
+
+      this.localStorage.clear('authenticationToken');
+      this.localStorage.clear('username');
+      this.localStorage.clear('refreshToken');
+      this.localStorage.clear('expireAt');
+  }
+
+  getUserName(){
+    return this.localStorage.retrieve('username');
+  }
+
+  getRefreshToken(){
+    return this.localStorage.retrieve('refreshToken');
+  }
+
+  isLoggedIn(): boolean{
+    return this.getJwtToken() != null;
   }
 }
